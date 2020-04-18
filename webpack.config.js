e26beb2e4b4ce2a51587fd6651ebd04ed525e3ca
wide8550 <wide8550@gmail.com>
2020-04-18 14:06:39 +0800

    fixed webpack can't run build

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,33 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    browsers: ['> .5% or last 2 versions'],
+                  },
+                  useBuiltIns: 'usage',
+                  corejs: {
+                    version: 3,
+                    proposals: true,
+                  },
+                  debug: true, //方便調試
+                },
+              ],
+            ],
+            plugins: [
+              [
+                '@babel/plugin-transform-runtime',
+                {
+                  corejs: false,
+                  regenerator: false,
+                },
+              ],
+            ],
+          },
         },
       },
     ],
